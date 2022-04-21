d7c41ab87d0895e7b4404eccb55eed2ecb44c6b3
zoeurk <zoeurk@gmail.com>
2022-04-21 10:29:18 +0200

    update

--- basex.c
+++ basex.c
@@ -86,11 +86,10 @@ ssize_t read_with_wrap(struct basex *b, int garbage,int fd)
 }
 void write_with_wrap(struct basex *b, ssize_t cwrap, ssize_t lwrap, int fd)
 {	static ssize_t _cwrap_ = 0, _lwrap_ = 1;
-	int v = 0,v_ = 0, v__ = 0;;
 	static ssize_t inc;
 	for( 	inc = 0;
 		inc < b->outdef && errno == 0;
-		lwrap != 0 && _lwrap_ == lwrap && _cwrap_ == cwrap && ((v = write(fd,"\n",1)) > 0,_lwrap_ = 1, _cwrap_ = 0),
+		lwrap != 0 && _lwrap_ == lwrap && _cwrap_ == cwrap && write(fd,"\n",1),_lwrap_ = 1, _cwrap_ = 0,
 		errno == 0 && _cwrap_ != 0 && _cwrap_ == cwrap && (write(fd," ",1),_cwrap_ = 0,_lwrap_++ ),
 		errno == 0 && b->output[inc] != '\0' && write(fd,&b->output[inc],1),
 		inc++,
